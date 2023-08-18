826536f3290774210aa118450d7b2adc98aa9661
Oleksandr Shyman <westfolt@gmail.com>
2023-08-18 07:35:00 +0300

    unit tests

--- OrderManagement.Tests.Unit/CreateCustomerRequestValidatorTests.cs
+++ OrderManagement.Tests.Unit/CreateCustomerRequestValidatorTests.cs
@@ -18,7 +18,7 @@ namespace OrderManagement.Tests.Unit
         [Test]
         public void ShouldNotReturnErrorsForCorrectData()
         {
-            var testData = CreateCorrectCustomer();
+            var testData = CreateCorrectCustomerRequest();
             var result = sut.Validate(testData);
             Assert.IsTrue(result.IsValid);
         }
@@ -27,7 +27,7 @@ namespace OrderManagement.Tests.Unit
         [TestCase("length", "First name cannot exceed 50 characters.")]
         public void ShouldReturnErrorForWrongFirstName(string test, string errorMessage)
         {
-            var testData = CreateCorrectCustomer();
+            var testData = CreateCorrectCustomerRequest();
 
             switch (test)
             {
@@ -44,7 +44,76 @@ namespace OrderManagement.Tests.Unit
             Assert.That(result.Errors[0].ErrorMessage, Is.EqualTo(errorMessage));
         }
 
-        private CreateCustomerRequest CreateCorrectCustomer()
+        [TestCase("empty", "Last name is required.")]
+        [TestCase("length", "Last name cannot exceed 50 characters.")]
+        public void ShouldReturnErrorForWrongLastName(string test, string errorMessage)
+        {
+            var testData = CreateCorrectCustomerRequest();
+
+            switch (test)
+            {
+                case "empty":
+                    testData.LastName = "";
+                    break;
+                case "length":
+                    testData.LastName = new string('a', 51);
+                    break;
+            }
+
+            var result = sut.Validate(testData);
+            Assert.IsFalse(result.IsValid);
+            Assert.That(result.Errors[0].ErrorMessage, Is.EqualTo(errorMessage));
+        }
+
+        [TestCase("empty", "Email is required.")]
+        [TestCase("length", "Email cannot exceed 50 characters.")]
+        [TestCase("not-email", "Email must be a valid email address.")]
+        public void ShouldReturnErrorForWrongEmail(string test, string errorMessage)
+        {
+            var testData = CreateCorrectCustomerRequest();
+
+            switch (test)
+            {
+                case "empty":
+                    testData.Email = "";
+                    break;
+                case "length":
+                    testData.Email = "longemail" + new string('a', 40) + "@test.com";
+                    break;
+                case "not-email":
+                    testData.Email = "invalid email";
+                    break;
+
+            }
+
+            var result = sut.Validate(testData);
+            Assert.IsFalse(result.IsValid);
+            Assert.That(result.Errors[0].ErrorMessage, Is.EqualTo(errorMessage));
+        }
+
+        [TestCase("empty", "Phone is required.")]
+        [TestCase("not-phone", "Invalid phone number format. It should be 12 digits long (066 included).")]
+        public void ShouldReturnErrorForWrongPhone(string test, string errorMessage)
+        {
+            var testData = CreateCorrectCustomerRequest();
+
+            switch (test)
+            {
+                case "empty":
+                    testData.Phone = "";
+                    break;
+                case "not-phone":
+                    testData.Phone = "+3123123123123123";
+                    break;
+
+            }
+
+            var result = sut.Validate(testData);
+            Assert.IsFalse(result.IsValid);
+            Assert.That(result.Errors[0].ErrorMessage, Is.EqualTo(errorMessage));
+        }
+
+        private CreateCustomerRequest CreateCorrectCustomerRequest()
         {
             return new CreateCustomerRequest
             {
---
+++ OrderManagement.Tests.Unit/CreateOrderValidatorTests.cs
@@ -0,0 +1,95 @@
+﻿using NUnit.Framework;
+using OrderManagement.Core.Models.Requests;
+using OrderManagement.Core.Validation;
+using OrderManagement.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagement.Tests.Unit
+{
+    public class CreateOrderValidatorTests
+    {
+        private CreateOrdRequestValidator sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            sut = new CreateOrdRequestValidator();
+        }
+
+        [Test]
+        public void ShouldNotReturnErrorsForCorrectData()
+        {
+            var testData = CreateCorrectOrderRequest();
+            var result = sut.Validate(testData);
+            Assert.IsTrue(result.IsValid);
+        }
+
+        [TestCase("empty", "Order Date is required")]
+        [TestCase("future", "Order Date cannot be in the future")]
+        public void ShouldReturnErrorForWrongOrderDate(string test, string errorMessage)
+        {
+            var testData = CreateCorrectOrderRequest();
+
+            switch (test)
+            {
+                case "empty":
+                    testData.OrderDate = new DateTime();
+                    break;
+                case "future":
+                    testData.OrderDate = testData.OrderDate.AddDays(1);
+                    break;
+            }
+
+            var result = sut.Validate(testData);
+            Assert.IsFalse(result.IsValid);
+            Assert.That(result.Errors[0].ErrorMessage, Is.EqualTo(errorMessage));
+        }
+
+        [TestCase("empty", "Order Number is required")]
+        [TestCase("length", "Order Number must be 10 characters or less")]
+        public void ShouldReturnErrorForWrongLastName(string test, string errorMessage)
+        {
+            var testData = CreateCorrectOrderRequest();
+
+            switch (test)
+            {
+                case "empty":
+                    testData.OrderNumber = "";
+                    break;
+                case "length":
+                    testData.OrderNumber = new string('a', 11);
+                    break;
+            }
+
+            var result = sut.Validate(testData);
+            Assert.IsFalse(result.IsValid);
+            Assert.That(result.Errors[0].ErrorMessage, Is.EqualTo(errorMessage));
+        }
+
+        public void ShouldReturnErrorForWrongCustomerId(string test, string errorMessage)
+        {
+            var testData = CreateCorrectOrderRequest();
+
+            testData.CustomerId = 0;
+
+            var result = sut.Validate(testData);
+            Assert.IsFalse(result.IsValid);
+            Assert.That(result.Errors[0].ErrorMessage, Is.EqualTo("Customer Id should be above zero"));
+        }
+
+        private CreateOrderRequest CreateCorrectOrderRequest()
+        {
+            return new CreateOrderRequest
+            {
+                OrderNumber = "123456789",
+                OrderDate = DateTime.Now.AddMinutes(-1),
+                CustomerId = 1,
+                Status = OrderStatus.Created
+            };
+        }
+    }
+}
